318ed260ffb8e1608009e7c47b07e5fd199fae8b
mmmnorthmark <mmmnorthmark@users.noreply.github.com>
2026-07-15 13:19:43 -0500

    Bump version to 0.6.0
    New feature: the server now ships its own OAuth authorization server.

--- src/version.ts
+++ src/version.ts
@@ -1,3 +1,3 @@
 // This should match the version in package.json or else it will break the CI workflow
 // You should not need to manually update this value, it will be managed using `pnpm version:sync` during release creation
-export const VERSION = '0.5.5';
+export const VERSION = '0.6.0';
